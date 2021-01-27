39fcca7c5cf2ce5faec1f27158220572406bc1cc
matcha1024 <matcha.exe@gmail.com>
2021-01-27 21:58:30 +0900

    add 404 and error pages

--- app.py
+++ app.py
@@ -45,5 +45,13 @@ def upload():
 
     return send_file("./note.zip", as_attachment = True, \
         attachment_filename = "note.zip")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template("error.html")
 if __name__ == '__main__':
     app.run()
